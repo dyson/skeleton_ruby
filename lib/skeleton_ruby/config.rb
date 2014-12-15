5d1ef800a699132ee232e51b3a3f38ce8111880a
Dyson Simmons <dysonsimmons@gmail.com>
2014-12-15 15:36:43 +1100

    Added inspect and respond_to_missing? to config.

--- lib/skeleton_ruby/config.rb
+++ lib/skeleton_ruby/config.rb
@@ -7,8 +7,16 @@ module SkeletonRuby
       yield(config) if block_given?
     end
 
+    def self.inspect
+      config.inspect
+    end
+
     def self.method_missing(method, *args, &block)
       config.send(method, *args, &block)
+    end
+
+    def self.respond_to_missing?(method, include_private_methods = false)
+      config.respond_to?(method) || super
     end
 
     private
