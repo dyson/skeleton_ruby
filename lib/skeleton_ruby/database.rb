04283161a8bdbfa865d10361978e38bf8fee93ba
Dyson Simmons <dysonsimmons@gmail.com>
2014-12-18 17:02:30 +1100

    Formatting.

--- lib/skeleton_ruby/database.rb
+++ lib/skeleton_ruby/database.rb
@@ -5,8 +5,8 @@ module SkeletonRuby
       # Set up active record Logger, load configuration and set database based on environment
       ActiveRecord::Base.logger = SkeletonRuby::Logger
 
-      configuration_dir = File.expand_path('../../../config', __FILE__)
-      database_configuration = YAML.load(File.read(File.join(configuration_dir, 'database.yml')))
+      configuration_dir = File.expand_path '../../../config', __FILE__
+      database_configuration = YAML.load File.read(File.join(configuration_dir, 'database.yml'))
 
       ActiveRecord::Base.configurations = database_configuration
       ActiveRecord::Base.establish_connection ENV['APP_ENV'].to_sym
